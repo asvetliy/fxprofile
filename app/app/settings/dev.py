acf2f67dfcba1d69313098e06a278dd91243bd5e
alex <alexandr.svetliy@gmail.com>
2021-04-04 18:35:07 +0300

    fixed dev config for local docker

--- app/app/settings/dev.py
+++ app/app/settings/dev.py
@@ -8,13 +8,14 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'fxprofile',
-        'USER': 'fxprofile_sdfmkg',
-        'PASSWORD': 'O4?36acb',
-        'HOST': '95.217.45.248',
+        'NAME': 'appdata',
+        'USER': 'root',
+        'PASSWORD': 'kA8rZ2cH2qW4oK7uK1uQ5aV5iX0nX7fK',
+        'HOST': os.environ.get('SQL_HOST', 'localhost'),
         'PORT': '3306',
     }
 }
+
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
