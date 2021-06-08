8128a744df69ff4bf851d0e222c83a0bfcfb3d44
alex <alexandr.svetliy@icloud.com>
2021-06-08 13:52:55 +0300

    fixed exchange price not exist

--- app/exchange/exchange.py
+++ app/exchange/exchange.py
@@ -1,18 +1,25 @@
 from .models import Prices
 
 
+def get_price_or_none(model: Prices, *args, **kwargs):
+    try:
+        return Prices.objects.get(*args, **kwargs)
+    except model.DoesNotExist:
+        return None
+
+
 class Exchange(object):
     @staticmethod
     def conv(amount: float, from_currency: str, to_currency: str) -> float:
         if from_currency == to_currency:
             return amount
         symbol = from_currency + to_currency
-        price = Prices.objects.get(SYMBOL=symbol)
+        price = get_price_or_none(Prices, SYMBOL=symbol)
         if price:
             return price.ASK * amount
         else:
             symbol = to_currency + from_currency
-            price = Prices.objects.get(SYMBOL=symbol)
+            price = get_price_or_none(Prices, SYMBOL=symbol)
             if price:
                 return (1 / price.ASK) * amount
         return amount
