bed6aefc6de360aeb7aee73ca6aa23d3fff66eb8
alex <alexandr.svetliy@icloud.com>
2021-06-24 18:23:06 +0300

    rockspay alert fix

--- app/fxprofile/static/js/plugins/tools.js
+++ app/fxprofile/static/js/plugins/tools.js
@@ -12,5 +12,8 @@ let tools = {
         align: align
       }
     });
+  },
+  maskCard: function (e) {
+    let n=e.textContent,l=n.replace(/\s+/g,"").replace(/[^0-9]/gi,"").match(/\d{4,16}/g),t=l&&l[0]||"",g=[];for(i=0,len=t.length;i<len;i+=4)g.push(t.substring(i,i+4));return g.length?g.join(" "):n;
   }
 };
