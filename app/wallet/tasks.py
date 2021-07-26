979f11412ba93ef36c3738bbb6055d6d11de0d18
alex <alexandr.svetliy@icloud.com>
2021-07-26 15:24:26 +0300

    expire deposit transaction only

--- app/wallet/tasks.py
+++ app/wallet/tasks.py
@@ -4,7 +4,7 @@ from .models import Transaction
 
 
 def close_expired_transactions():
-    current_transactions = Transaction.objects.filter(status_id=2)
+    current_transactions = Transaction.objects.filter(status_id=2, type_id=1)
     for transaction in current_transactions:
         duration = datetime.now().timestamp() - transaction.created_at.timestamp()
         if duration > 86400:
